4ecf555de0a444cb715a125ce4158181254e5582
Amar Teragundi <amarteragundi@gmail.com>
2019-05-09 19:17:45 +0530

    changing App function to class

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+
+class App extends React.Component {
+
+    onSearchSubmit(term){
+        console.log(term);
+
+//         <div className="ui card">
+//   <a className="image" href="#">
+//     <img src="/images/avatar/large/steve.jpg" />
+//   </a>
+//   <div className="content">
+//     <a className="header" href="#">Steve Jobes</a>
+//     <div className="meta">
+//       <a>Last Seen 2 days ago</a>
+//     </div>
+//   </div>
+// </div>
+
+    }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
