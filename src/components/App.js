88adfbd7d1724d818124b8f29164f0e2de925316
Amar Teragundi <amarteragundi@gmail.com>
2019-05-09 16:04:28 +0530

    calling searchbar in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-
-}
+  return (
+    <div>
+      HI
+      <SearchBar />
+    </div>
+  );
+};
 
 export default App;
