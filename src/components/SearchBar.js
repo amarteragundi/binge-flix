4d7a3ebc4bafe87da68d89acc5aa5d678c091e73
Amar Teragundi <amarteragundi@gmail.com>
2019-05-09 16:41:13 +0530

    fix

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    state={term: ""}
+  state = { term: "" };
   render() {
     return (
       <div id="searchBar">
         <form>
-        <div className="ui massive fluid action input">
-            <input type="text" value={this.state.term} placeholder="Search..." onchange={(e) => this.setState({term: e.target.value})} />
+          <div className="ui massive fluid action input">
+            <input
+              type="text"
+              value={this.state.term}
+              placeholder="Search..."
+              onChange={e => this.setState({ term: e.target.value })}
+            />
             <div className="ui button">Search</div>
-        </div>
-          </form>
+          </div>
+        </form>
       </div>
     );
   }
