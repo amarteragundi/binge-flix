a16252d7d5c588c84998435d24f8e899d8a432b4
Amar Teragundi <amarteragundi@gmail.com>
2019-05-10 15:53:17 +0530

    adding show description

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
       params: { q: term, embed: "cast" }
     });
     let data = response.data;
-    this.setState({ title: data.name, rating: data.rating.average, poster:data.image.medium });
+    this.setState({ title: data.name, rating: data.rating.average, poster:data.image.medium, description: data.summary });
   };
 
   componentDidUpdate(){
@@ -53,7 +53,7 @@ class App extends React.Component {
                 }}
               >
                 <ShowDescription
-                  description={`<p>After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only "meta-human" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash!</p>`}
+                  description={this.state.description}
                 />
                 <div className="ui grid" style={{ marginTop: "30px" }}>
                   <OtherDetails icon="language" content="English" />
