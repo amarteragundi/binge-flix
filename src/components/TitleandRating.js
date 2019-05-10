ec429c85ff108e295d82a13c428672dca16720c9
Amar Teragundi <amarteragundi@gmail.com>
2019-05-10 11:53:21 +0530

    component for title

---
+++ src/components/TitleandRating.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const TitleandRating = (props) => {
+    return (
+        <h1
+            style={{
+                color: "white",
+                textTransform: "uppercase"
+            }}
+        >
+            {props.title} |
+            <span
+                style={{
+                    fontSize: "14px",
+                    verticalAlign: "middle"
+                }}
+            >
+                <i className="heart icon" style={{ color: "#ff6d75" }} />
+                {props.rating}
+            </span>
+        </h1>
+    );
+};
+
+export default TitleandRating;
