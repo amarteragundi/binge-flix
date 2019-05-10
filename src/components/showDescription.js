0316422874267bc3e4514e9c05910e66be0da0c6
Amar Teragundi <amarteragundi@gmail.com>
2019-05-10 15:53:01 +0530

    refactoring

--- src/components/showDescription.js
+++ src/components/showDescription.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const ShowDescription = props => {
-    return <div dangerouslySetInnerHTML={{__html: getHTML(props.description).__html}} />;
+  return (
+    <div dangerouslySetInnerHTML={{ __html: getHTML(props.description) }} />
+  );
 };
 
-function getHTML(description){
-    return {__html: description}
-  }
+function getHTML(description) {
+  let desc = { __html: description };
+  return desc.__html;
+}
 export default ShowDescription;
