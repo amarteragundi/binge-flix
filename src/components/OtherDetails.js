fdcaad38c32245d38cd7f4f55d31a5a33b9c9f94
Amar Teragundi <amarteragundi@gmail.com>
2019-05-13 09:23:32 +0530

    rendering other details

--- src/components/OtherDetails.js
+++ src/components/OtherDetails.js
@@ -1,14 +1,71 @@
-import React from 'react';
+import React from "react";
 
-const OtherDetails = (props) => {
+const OtherDetails = props => {
+    let details, otherDetails;
+    if (props.details) {
+        details = props.details;
+        let items = ["language", "film", "calendar", "circle"];
+        otherDetails = details.map((item, key) => {
+            if (Array.isArray(item, key)) {
+                return (
+                    <div key={key} className="four wide column">
+                        <h3 className="ui header">
+                            <i className="large icons">
+                                <i className={`${items[key]} icon`} />
+                            </i>
+                            {item.join(", ")}
+                        </h3>
+                    </div>
+                );
+            } else if (
+                typeof item === "object" &&
+                item.constructor === Object
+            ) {
+                let detail = "";
+                for (let property in item) {
+                    if (typeof item[property] == "string") {
+                        if (item[property]) detail += item[property];
+                    } else {
+                        if (item[property])
+                            detail += ` ${item[property].join()}`;
+                    }
+                }
+                return (
+                    <div key={key} className="four wide column">
+                        <h3 className="ui header">
+                            <i className="large icons">
+                                <i className={`${items[key]} icon`} />
+                            </i>
+                            {detail}
+                        </h3>
+                    </div>
+                );
+            } else {
+                let iconColor;
+                if (items[key] == "circle")
+                    item == "Ended"
+                        ? (iconColor = "red")
+                        : (iconColor = "green");
+                return (
+                    <div key={key} className="four wide column">
+                        <h3 className="ui header">
+                            <i className="large icons">
+                                <i
+                                    className={`${items[key]} icon`}
+                                    style={{ color: iconColor }}
+                                />
+                            </i>
+                            {item}
+                        </h3>
+                    </div>
+                );
+            }
+        });
+    }
+    // let [language, genre, schedule, status] = props;
     return (
-        <div className="four wide column">
-            <h3 className="ui header">
-                <i className="large icons">
-                    <i className={`${props.icon} icon`} />
-                </i>
-                {props.content}
-            </h3>
+        <div className="ui grid" style={{ marginTop: "30px" }}>
+            {otherDetails}
         </div>
     );
 };
