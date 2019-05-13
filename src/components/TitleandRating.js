0d8f9cd30960d9b3209c1245c6f7a413ffa28249
Amar Teragundi <amarteragundi@gmail.com>
2019-05-13 10:08:25 +0530

    hiding the heart icon

--- src/components/TitleandRating.js
+++ src/components/TitleandRating.js
@@ -1,25 +1,32 @@
 import React from "react";
 
 const TitleandRating = props => {
-    return (
-        <h1
-            style={{
-                color: "white",
-                textTransform: "uppercase"
-            }}
-        >
-            {props.title} |
-            <span
+    if (props.title) {
+        return (
+            <h1
                 style={{
-                    fontSize: "14px",
-                    verticalAlign: "middle"
+                    color: "white",
+                    textTransform: "uppercase"
                 }}
             >
-                <i className="heart icon" style={{ color: "#ff6d75", marginLeft:"5px" }} />
-                {props.rating}
-            </span>
-        </h1>
-    );
+                {props.title} |
+                <span
+                    style={{
+                        fontSize: "14px",
+                        verticalAlign: "middle"
+                    }}
+                >
+                    <i
+                        className="heart icon"
+                        style={{ color: "#ff6d75", marginLeft: "5px" }}
+                    />
+                    {props.rating}
+                </span>
+            </h1>
+        );
+    } else {
+        return <div />;
+    }
 };
 
 export default TitleandRating;
