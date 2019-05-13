f6c04722100672f04c52c71f772f20ad65121cc0
Amar Teragundi <amarteragundi@gmail.com>
2019-05-13 09:23:48 +0530

    rendering other details

--- src/components/App.js
+++ src/components/App.js
@@ -10,104 +10,101 @@ import Cast from "./Cast";
 import MyState from "./State";
 
 class App extends React.Component {
-  state = MyState;
-  onSearchSubmit = async term => {
-    console.log(term);
-    const response = await tvmaze.get("/singlesearch/shows", {
-      params: { q: term, embed: "cast" }
-    });
-    let data = response.data;
-    this.setState({
-      title: data.name,
-      rating: data.rating.average,
-      poster: data.image.medium,
-      description: data.summary,
-      OtherDetails: {
-        language: data.language,
-        genre: data.genres,
-        schedule: data.schedule
-      }
-    });
-  };
-
-  componentDidUpdate() {
-    this.getGradient();
-  }
-
-  getGradient() {
-    let options = {
-      angle: 0, // gradient angle in degrees
-      steps: 5 // number of steps
+    state = MyState;
+    onSearchSubmit = async term => {
+        console.log(term);
+        const response = await tvmaze.get("/singlesearch/shows", {
+            params: { q: term, embed: "cast" }
+        });
+        let data = response.data;
+        this.setState({
+            title: data.name,
+            rating: data.rating.average,
+            poster: data.image.medium,
+            description: data.summary,
+            otherDetails: [
+                data.language,
+                data.genres,
+                data.schedule,
+                data.status
+            ]
+        });
     };
 
-    imageToGradient(this.state.poster, options, function(err, cssGradient) {
-      console.log(cssGradient);
-      let el = document.querySelector("body");
-      el.style.background = cssGradient;
-    });
-  }
+    componentDidUpdate() {
+        this.getGradient();
+    }
 
-  render() {
-    return (
-      <div>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <div style={{ margin: "0px auto", padding: "30px" }}>
-          <div className="ui grid" style={{}}>
-            <div className="twelve wide column">
-              <TitleandRating
-                title={this.state.title}
-                rating={this.state.rating}
-              />
-              <div
-                style={{
-                  minHeight: "220px",
-                  fontSize: "20px",
-                  lineHeight: "56pt"
-                }}
-              >
-                <ShowDescription description={this.state.description} />
-                <div className="ui grid" style={{ marginTop: "30px" }}>
-                  <OtherDetails icon="language" content="English" />
-                  <OtherDetails icon="film" content="Comedy, Romantic" />
-                  <OtherDetails
-                    icon="calendar outline"
-                    content="20:00, Thursday"
-                  />
-                  <OtherDetails icon="circle" content="Running" />
+    getGradient() {
+        let options = {
+            angle: 0, // gradient angle in degrees
+            steps: 5 // number of steps
+        };
+
+        imageToGradient(this.state.poster, options, function(err, cssGradient) {
+            console.log(cssGradient);
+            let el = document.querySelector("body");
+            el.style.background = cssGradient;
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <div style={{ margin: "0px auto", padding: "30px" }}>
+                    <div className="ui grid" style={{}}>
+                        <div className="twelve wide column">
+                            <TitleandRating
+                                title={this.state.title}
+                                rating={this.state.rating}
+                            />
+                            <div
+                                style={{
+                                    minHeight: "220px",
+                                    fontSize: "20px",
+                                    lineHeight: "56pt"
+                                }}
+                            >
+                                <ShowDescription
+                                    description={this.state.description}
+                                />
+                                 <OtherDetails
+                                        details={this.state.otherDetails}
+                                    />
+                            </div>
+                        </div>
+                        <div
+                            className="four wide column"
+                            style={{
+                                display: "table-cell",
+                                textAlign: "center",
+                                verticalAlign: "middle"
+                            }}
+                        >
+                            <img
+                                src={this.state.poster}
+                                style={{
+                                    boxShadow:
+                                        "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="sixteen wide column">
+                        <h1>Main Cast</h1>
+                        <div className="ui six column grid">
+                            <Cast
+                                img="http://static.tvmaze.com/uploads/images/medium_portrait/95/239388.jpg"
+                                charName="Bartholomew Henry 'Barry' Allen / The Flash"
+                                name="Grant Gustin"
+                            />
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-            <div
-              className="four wide column"
-              style={{
-                display: "table-cell",
-                textAlign: "center",
-                verticalAlign: "middle"
-              }}
-            >
-              <img
-                src={this.state.poster}
-                style={{
-                  boxShadow:
-                    "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
-                }}
-              />
-            </div>
-          </div>
-          <div className="sixteen wide column">
-            <h1>Main Cast</h1>
-            <div className="ui six column grid">
-              <Cast
-                img="http://static.tvmaze.com/uploads/images/medium_portrait/95/239388.jpg"
-                charName="Bartholomew Henry 'Barry' Allen / The Flash"
-                name="Grant Gustin"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
